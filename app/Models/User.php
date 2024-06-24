0fcc88f66d2d2481f9390a01210e10884fb28c36
LuisIrua23 <lairua@soy.sena.edu.co>
2024-06-24 18:35:50 -0500

    luis

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;jjjj
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
